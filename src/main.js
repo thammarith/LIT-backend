3eca7cfad5bd06999aeed21299dabcad7176d0d8
Thammarith <thammarith@users.noreply.github.com>
2019-12-17 00:27:30 +0700

    Add vue-toasted

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Toasted from "vue-toasted";
 
 Vue.config.productionTip = false;
+
+Vue.use(Toasted);
 
 new Vue({
   render: h => h(App)
