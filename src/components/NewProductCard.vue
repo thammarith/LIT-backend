d609a56d12c1958924d9075e49caa63ea112c054
Thammarith <thammarith@users.noreply.github.com>
2019-12-17 00:26:34 +0700

    Create NewProductCard.vue

---
+++ src/components/NewProductCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <Card @click.native="addNewProduct" class="card">
+    +
+  </Card>
+</template>
+
+<script>
+import Card from "./Card";
+
+export default {
+  components: {
+    Card
+  },
+  methods: {
+    addNewProduct() {
+      this.$emit("add-new-product");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  align-items: center;
+  border: 0.5rem dashed #ccc;
+  color: #ccc;
+  display: flex;
+  font-size: 6rem;
+  font-weight: 500;
+  justify-content: center;
+
+  &:hover {
+    border: 0.5rem dashed #0c0;
+    color: #0c0;
+    cursor: pointer;
+  }
+}
+</style>
