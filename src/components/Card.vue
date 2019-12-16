1988641773c6bc4095f18edc0d6ac1e5b93b4a7b
Thammarith <thammarith@users.noreply.github.com>
2019-12-17 00:26:28 +0700

    Create Card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  background: #fafafa;
+  // border: 1px solid black;
+  height: 16rem;
+  // margin-left: 1rem;
+  margin-top: 2rem;
+  width: 10rem;
+}
+</style>
